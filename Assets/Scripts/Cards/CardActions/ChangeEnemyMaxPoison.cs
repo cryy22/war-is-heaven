ef253afe5f1a077e05275b9f65b810fa64567e51
cryy22 <cry@anon-f77397bb01ed4c01.art>
2022-11-13 19:10:41 -0800

    enemy increases their poisonousness intent

---
+++ Assets/Scripts/Cards/CardActions/ChangeEnemyMaxPoison.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using WarIsHeaven.Audio;
+
+namespace WarIsHeaven.Cards.CardActions
+{
+    [CreateAssetMenu(
+        fileName = "ChangeEnemyMaxPoison",
+        menuName = "Scriptable Objects/Card Actions/Change Enemy Max Poison"
+    )]
+    public class ChangeEnemyMaxPoison : CardAction
+    {
+        public override void Invoke(Context context, int magnitude = 1)
+        {
+            if (magnitude > 0)
+                FXPlayer.Instance.PlayHealSound();
+            else if (magnitude < 0)
+                FXPlayer.Instance.PlayGunshot();
+
+            context.EnemyUnit.Poisonous.UpdateMaxValue(magnitude);
+        }
+    }
+}
